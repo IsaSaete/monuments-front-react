4b39ce20d5d5d832adbda61cd5d53f1d98ca5169
Isabel Sáenz <isasaete@gmail.comn>
2025-04-07 23:16:23 +0200

    Modify the title name classes

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -4,8 +4,8 @@ const Header: React.FC = () => {
   return (
     <div className="header">
       <h1 className="title">
-        <span className="app__title">Monuments</span>
-        <span className="app__subtitle"> of the world</span>
+        <span className="title__main">Monuments</span>
+        <span className="title__secondary"> of the world</span>
       </h1>
     </div>
   );
