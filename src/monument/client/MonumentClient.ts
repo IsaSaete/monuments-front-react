946554c6f0b0b19f7eb798c21304691c104f8297
Isabel Sáenz <isasaete@gmail.comn>
2025-04-10 18:42:22 +0200

    Modify type promise MonumentClient

--- src/monument/client/MonumentClient.ts
+++ src/monument/client/MonumentClient.ts
@@ -1,15 +1,15 @@
-import mapMonumentsDtotoMonuments from "../dto/mapper";
-import MonumentDto from "../types";
+import mapMonumentsDtotoMonuments from "../dto/mappers";
+import Monument from "../types";
 import MonumentClientStructure from "./types";
 
 class MonumentClient implements MonumentClientStructure {
   private apiUrl = import.meta.env.VITE_API_URL;
 
-  public getMonument = async (): Promise<MonumentDto[]> => {
+  public getMonument = async (): Promise<Monument[]> => {
     const response = await fetch(`${this.apiUrl}/monuments`);
 
     const { monuments } = (await response.json()) as {
-      monuments: MonumentDto[];
+      monuments: Monument[];
     };
 
     return mapMonumentsDtotoMonuments(monuments);
