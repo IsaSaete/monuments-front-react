1987b82793f9d16860da013a5c9ba1e0e2116fd7
Isabel Sáenz <isasaete@gmail.comn>
2025-04-13 09:51:54 +0200

    Show all the information about the monument on the card

--- src/monument/components/MonumentCard/MonumentCard.tsx
+++ src/monument/components/MonumentCard/MonumentCard.tsx
@@ -5,19 +5,17 @@ interface MonumentCardProps {
   monument: Monument;
 }
 
-const MonumentCard: React.FC<MonumentCardProps> = ({ monument }) => {
+const MonumentCard: React.FC<MonumentCardProps> = ({
+  monument: { imageUrl, imageAlt, name, description, city, country },
+}) => {
   return (
     <article className="monument-card">
-      <img
-        className="monument-card__image"
-        src={monument.imageUrl}
-        alt={monument.imageAlt}
-      />
-      <h2 className="monument-card__name">{monument.name}</h2>
-      <p className="monument-card__description">{monument.description}</p>
+      <img className="monument-card__image" src={imageUrl} alt={imageAlt} />
+      <h2 className="monument-card__name">{name}</h2>
+      <p className="monument-card__description">{description}</p>
       <div className="monument-card__location">
-        <span>{monument.city}</span>
-        <span>{monument.country}</span>
+        <span>{city}</span>
+        <span>{country}</span>
       </div>
     </article>
   );
